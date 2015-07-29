4b5a9be0b28e2804e76e7ffcd2fab98df1b3f3dd
Stefan Balsiger <ste.balsiger@bluewin.ch>
2015-07-29 15:12:43 +0200

    SAVE

--- app/performance_view/performance_view.js
+++ app/performance_view/performance_view.js
@@ -10,28 +10,38 @@ angular.module('myApp.performance_view', ['ngRoute'])
 }])
 
     .controller('performanceCtrl', ['$scope', function($scope) {
-      $scope.master={};
+        $scope.master={};
 
         $scope.user={};
 
-      $scope.update = function(user) {
-        $scope.master = angular.copy(user);
-      };
+        $scope.update = function(user) {
+            $scope.master = angular.copy(user);
+            };
 
 
     }])
 
     .controller('skillsCtrl', function($scope) {
       $scope.skills = [
-          {id: 1, label: 'Existing and acquired professional knowledge', chapter: "dev_exp"},
-          {id: 2, label: 'Existing and acquired professional skills', chapter: "dev_exp"},
-          {id: 3, label: 'Practical implementation', chapter: "dev_exp"},
-          {id: 4, label: 'Knowledge transfer', chapter: "dev_exp"},
-          {id: 5, label: 'Diligent', chapter: "diligence_reliability"},
-          {id: 6, label: 'Exact', chapter: "diligence_reliability"}
+          {skill_id: 1, label: 'Existing and acquired professional knowledge', chapter: "dev_exp"},
+          {skill_id: 2, label: 'Existing and acquired professional skills', chapter: "dev_exp"},
+          {skill_id: 3, label: 'Practical implementation', chapter: "dev_exp"},
+          {skill_id: 4, label: 'Knowledge transfer', chapter: "dev_exp"},
+          {skill_id: 5, label: 'Diligent', chapter: "diligence_reliability"},
+          {skill_id: 6, label: 'Exact', chapter: "diligence_reliability"}
         ];
 
 
     })
+
+    .controller('evaluationCtrl', function($scope){
+        $scope.ratingCheckboxModel = {
+            value: false
+        };
+
+        $scope.evaluation = [
+            {evaluation_id: 1, skill_id: 5, rating: 3}
+        ];
+})
 
 ;
